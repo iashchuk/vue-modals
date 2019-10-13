7b4783fc606dac9804440d53ada4a14b641eafc5
iashchuk <221box@list.ru>
2019-10-13 23:41:45 +0300

    Добавляет модальное окно с формой

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,25 @@
     <div class="app__content wrapper-content">
       <section class="app__section">
         <div class="app__container container">
-          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
-          <Modal v-show="modalFirst" :title="title" @closeModal="modalFirst=false">
+          <!-- Basic modal -->
+          <button class="btn btnPrimary" @click="basicModal = !basicModal">Show basic modal</button>
+          <Modal v-show="basicModal" title="Basic Modal" @closeModal="basicModal=false">
             <div slot="body">
               <p>Text</p>
               <button>Click</button>
             </div>
+          </Modal>
+
+          <!-- Form modal -->
+          <button class="btn btnPrimary" @click="formModal.show = !formModal.show">Show form modal</button>
+          <Modal v-show="formModal.show" title="Form Modal" @closeModal="formModal.show=false">
+            <form slot="body" @submit.prevent="submitForm">
+              <label>Name:</label>
+              <input type="text" v-model="formModal.name" required/>
+              <label>E-mail:</label>
+              <input type="text" v-model="formModal.email" required />
+              <button type="submit" class="btn btnPrimary">Submit!</button>
+            </form>
           </Modal>
         </div>
       </section>
@@ -25,9 +38,24 @@ export default {
   },
   data() {
     return {
-      modalFirst: false,
-      title: "Modals App"
+      basicModal: false,
+      formModal: {
+        show: false,
+        name: "",
+        email: ""
+      }
     };
+  },
+  methods: {
+    submitForm() {
+      console.log({
+        name: this.formModal.name,
+        email: this.formModal.email
+      });
+
+      this.formModal.name = "";
+      this.formModal.email = "";
+    }
   }
 };
 </script>
